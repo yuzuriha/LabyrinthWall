24501ff1929b2828634598c12a36e813729f2468
yuzuriha <yuzuriha@users.noreply.github.com>
2014-08-31 04:42:58 +0900

    プロジェクト追加

---
+++ LabyrinthWall/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LabyrinthWall
+//
+//  Created by yuzuriha on 2014/08/31.
+//  Copyright (c) 2014年 yuzuriha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
